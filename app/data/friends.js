5dad001660d50f3024ee9b9cf30a67848dbad7c2
Malorie Lara <malorielara@users.noreply.github.com>
2018-09-19 18:17:51 -0500

    adding friends.js -- rugrats characters hah

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// Rugrats gang
+
+var friendsArray = [
+	{
+		name: 'Tommy',
+		photo: 'https://vignette.wikia.nocookie.net/rugrats-series/images/9/9c/Tommy_Pickles..png/revision/latest?cb=20180629055802',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'2',
+			'5',
+			'1',
+			'2',
+			'3',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chucky',
+		photo: 'https://vignette.wikia.nocookie.net/nickelodeon/images/a/ad/Chuckie_Finster.png/revision/latest?cb=20150411032458',
+		scores: [
+			'3',
+			'2',
+			'4',
+			'4',
+			'5',
+			'2',
+			'3',
+			'3',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Phil',
+		photo: '',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'4',
+			'1',
+			'1',
+			'2',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lil',
+		photo: 'https://img00.deviantart.net/8ad2/i/2012/171/f/a/roger_klotz_and_stinky_by_luthien_black-d2oa0qd.jpg',
+		scores: [
+			'5',
+			'3',
+			'1',
+			'2',
+			'2',
+			'2',
+      '3',
+			'1',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Angelica',
+		photo: 'https://img.sharetv.com/shows/characters/large/doug.judith_judy_anastasia_funnie.jpg',
+		scores: [
+			'1',
+			'5',
+			'4',
+			'3',
+			'3',
+			'3',
+			'4',
+			'4',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Susie',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dill',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/8/8c/581278-image_2_quailman_large.jpeg/revision/latest?cb=20111006235840',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Raptar',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/e/e3/Beebe_Bluff.png/revision/latest?cb=20180516112344',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Spike',
+		photo: 'https://vignette.wikia.nocookie.net/doug/images/0/08/Porkchop.png/revision/latest?cb=20180531191333',
+		scores: [
+			'5',
+			'3',
+			'3',
+			'2',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Kimi',
+		photo: 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/connie-benge-dougs-1st-movie-3.9.jpg',
+		scores: [
+			'2',
+			'5',
+			'5',
+			'3',
+			'3',
+			'2',
+			'3',
+			'5',
+			'3',
+			'1'
+		]
+	}
+];
