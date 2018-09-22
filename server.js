0eec2e7e5be8e96eda85caaf20342cd474bd9b60
Malorie Lara <malorielara@users.noreply.github.com>
2018-09-22 12:52:45 -0500

    server.js and fixed stupid mistakes in questions id

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
+
+
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
